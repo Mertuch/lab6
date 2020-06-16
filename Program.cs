9aafedc17ccbf267449c9246d72e1e97ec94dbe4
Mertuch <mertucha@o2.pl>
2020-06-16 20:03:20 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Silnia s = new Silnia(4);
+
+            Console.WriteLine("Stworzyłem obiekt silnia");
+            Console.WriteLine(s.Oblicz());
+            Console.WriteLine("PRES ENY KII TU KONTINIU\n");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Silnia.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    public class Silnia
+    {
+
+        private int n;
+        
+
+        public Silnia(int n)
+        {
+            if (n < 0) { n = 0; }
+            this.n = n;
+
+        }
+
+        public int Oblicz()
+        {
+            if (n == 0) return 1;
+            int temp = 1;
+            for(int i = 1; i <= this.n; i++)
+            {
+                temp *= i;
+            }
+            return temp;
+        }
+
+
+    }
+}
