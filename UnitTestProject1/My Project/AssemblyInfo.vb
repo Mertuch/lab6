7bd792261fa74325bfb76be6d9274034d342b1f7
Mertuch <mertucha@o2.pl>
2020-06-16 20:04:28 +0200

    lab6

---
+++ UnitTestProject1/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("UnitTestProject1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("UnitTestProject1")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("fcc7aa10-7a4c-412b-864c-f2007f60b809")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab6;
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Silnia_0()
+        {
+            Silnia s = new Silnia(0);
+            Assert.AreEqual(s.Oblicz(), 1);
+        }
+        [TestMethod]
+        public void Silnia_1()
+        {
+            Silnia s = new Silnia(0);
+            Assert.AreEqual(s.Oblicz(), 1);
+        }
+        [TestMethod]
+        public void Silnia_5()
+        {
+            Silnia s = new Silnia(5);
+            Assert.AreEqual(s.Oblicz(), 120);
+        }
+    }
+}
